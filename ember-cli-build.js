90aa4dc011c3c80e72252bf44cd3734e1afb66d7
Evgeniy Khaberev <ehaberev@yandex.ru>
2017-08-01 17:26:49 +0500

    Add `broccoli-jscs`

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,11 @@ var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   var app = new EmberAddon(defaults, {
-    // Add options here
+    jscsOptions: {
+      enabled: true,
+      esnext: true,
+      configPath: './.jscsrc'
+    }
   });
 
   /*
